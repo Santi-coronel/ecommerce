a23a7567952e572119becb85749343cad64c0e87
Santi-coronel <santiagocoronel2005@gmail.com>
2025-09-30 19:54:00 -0300

    Delete Downloads/essential directory

--- Downloads/essential/src/main.js
+++
@@ -1,6 +0,0 @@
-import './styles/styles.css';
-import { initApp } from './app.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  initApp();
-});
--- Downloads/essential/src/utils/api.js
+++
@@ -1,5 +0,0 @@
-export async function fetchProducts() {
-  const res = await fetch('/src/data/products.json');
-  if (!res.ok) throw new Error('No se pudieron cargar los productos');
-  return await res.json();
-}

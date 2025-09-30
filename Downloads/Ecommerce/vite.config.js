ae8d414e68e62a1f1ed30f7f1e363c274d22fe97
santi-coronel <santiagocoronel2005@gmail.com>
2025-09-30 20:43:33 -0300

    fix: Corregida la ruta base de Vite y deploy

--- Downloads/Ecommerce/vite.config.js
+++ Downloads/Ecommerce/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Establece la ruta base para que coincida con el nombre de tu repositorio
-  base: '/ecommerce/',
+  // Establece la ruta base para GitHub Pages (nombre del repositorio)
+  base: '/ecommerce/', 
+  // Le dice a Vite que el directorio raíz del proyecto es donde está este archivo
+  root: './', 
 });
